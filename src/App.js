5a3b3803b58b42386cba335b0ede8a56390306af
freddiebell88 <frederikcwbell@gmail.com>
2023-11-28 17:01:53 -0500

    book titles showing on page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css'
-import bookData from './book-data.json'
+import "./App.css";
+import bookData from "./book-data.json";
 
 function App() {
   return (
     <div>
       <h1>Freeshelf</h1>
+      <div className="book-data">
+        {bookData.map((book) => (
+          <DataDisplay title={book.title} />
+        ))}
+      </div>
       {/* Use bookData to show books on the page*/}
     </div>
-  )
+  );
 }
 
-export default App
+function DataDisplay(props) {
+  // const [expanded, setExpanded] = useState(false);
+  // const handleClick = () => {
+  //   setExpanded(!expanded);
+  // };
+  return (
+    <div>
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
+export default App;
