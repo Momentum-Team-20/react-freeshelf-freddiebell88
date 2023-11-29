4df0b33bf11f935138c07f448b370a70a6a46871
freddiebell88 <frederikcwbell@gmail.com>
2023-11-29 10:25:30 -0500

    imported css and started formatting

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
             title={book.title}
             author={book.author}
             shortDesc={book.shortDescription}
+            cover={book.coverImageUrl}
             url={book.url}
             publisher={book.publisher}
             date={book.publicationDate}
@@ -30,17 +31,18 @@ function DataDisplay(props) {
     setExpanded(!expanded);
   };
   return (
-    <div>
+    <div className="book">
       <h3>{props.title}</h3>
       <p>{props.author}</p>
       <p>{props.shortDesc}</p>
+      <img className="book-cover" src={props.cover} alt="book cover" />
       <button onClick={handleClick}>
         {expanded ? "show less" : "show more"}
       </button>
-      {expanded && <p>{props.url}</p>}
-      {expanded && <p>{props.publisher}</p>}
-      {expanded && <p>{props.date}</p>}
-      {expanded && <p>{props.detailedDesc}</p>}
+      {expanded && <p>URL: {props.url}</p>}
+      {expanded && <p>Publiser: {props.publisher}</p>}
+      {expanded && <p>Publication Date: {props.date}</p>}
+      {expanded && <p>Full Description: {props.detailedDesc}</p>}
     </div>
   );
 }
